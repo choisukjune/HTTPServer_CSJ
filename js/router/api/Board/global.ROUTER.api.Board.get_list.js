dc5ca781192ecc66f9dcb34716e0c3980f576c0c
choisukjune <sukjune.choi@gmail.com>
2018-02-26 12:17:29 +0900

    asdf

--- js/router/api/Board/global.ROUTER.api.Board.get_list.js
+++ js/router/api/Board/global.ROUTER.api.Board.get_list.js
@@ -1,4 +1,4 @@
-/global.ROUTER.api.Board.get_list = function( req, res ){
+global.ROUTER.api.Board.get_list = function( req, res ){
 
 	var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	req.url ).replace(/^.*\?/, '') );
 	global.api.Session.session_check(req, res, _q.sid, function( result ){
