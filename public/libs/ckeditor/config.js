002ca3f08581b4a33a9a3bab0893d76879541446
choisukjune <sukjune.choi@gmail.com>
2018-02-28 00:47:30 +0900

    xcvxcv

--- public/libs/ckeditor/config.js
+++ public/libs/ckeditor/config.js
@@ -3,8 +3,23 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+ CKEDITOR.editorConfig = function( config ) {
+ 	config.toolbarGroups = [
+ 		{ name: 'document', groups: [ 'document', 'mode', 'doctools' ] },
+ 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+ 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+ 		{ name: 'forms', groups: [ 'forms' ] },
+ 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+ 		{ name: 'links', groups: [ 'links' ] },
+ 		{ name: 'tools', groups: [ 'tools' ] },
+ 		'/',
+ 		{ name: 'styles', groups: [ 'styles' ] },
+ 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+ 		{ name: 'colors', groups: [ 'colors' ] },
+ 		{ name: 'insert', groups: [ 'insert' ] },
+ 		{ name: 'others', groups: [ 'others' ] },
+ 		{ name: 'about', groups: [ 'about' ] }
+ 	];
+
+ 	config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,SelectAll,Scayt,About,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,BidiLtr,BidiRtl,Flash,PageBreak,Iframe,Language,Format,Smiley,Replace,Subscript,Anchor';
+ };
