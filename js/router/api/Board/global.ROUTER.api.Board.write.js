cc6a035b7a49b2241844dd235fb050eca8ba6c12
choisukjune <sukjune.choi@gmail.com>
2018-03-06 00:47:05 +0900

    sdfsdfsdf

--- js/router/api/Board/global.ROUTER.api.Board.write.js
+++ js/router/api/Board/global.ROUTER.api.Board.write.js
@@ -48,7 +48,7 @@ global.ROUTER.api.Board.write = function( req, res ){
 
 						db0.collection("notice").insert(doc,function(d){
 							console.log( d )
-							global.api.Response.res_200_ok_String( req, res, JSON.stringify( d );
+							global.api.Response.res_200_ok_String( req, res, JSON.stringify( d ) );
 							db.close();
 
 						});
