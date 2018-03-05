139c198c89e2c1f92fea08d0a800d654acd4910c
choisukjune <sukjune.choi@gmail.com>
2018-03-05 11:29:25 +0900

    asdfasdf

--- js/router/api/Board/global.ROUTER.api.Board.detail_view_by__id.js
+++ js/router/api/Board/global.ROUTER.api.Board.detail_view_by__id.js
@@ -19,7 +19,7 @@ global.ROUTER.api.Common.detail_view_by__id = function( req, res ){
 				console.log( _q._id  )
 				var db0 = db.db('board');
 
-				db0.collection("notice").find( { _id : _q._id } ).toArray(function(err, doc){
+				db0.collection("notice").find( { _id : _q._id * 1 } ).toArray(function(err, doc){
 					console.log( doc)
 					global.api.Response.res_200_ok_String( req, res, JSON.stringify( doc ));
 					db.close();
