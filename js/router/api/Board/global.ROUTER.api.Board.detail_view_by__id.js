37d6ee7413b9882f570a63ce56b563efd47dd0eb
choisukjune <sukjune.choi@gmail.com>
2018-03-05 11:25:16 +0900

    asdfasdf

--- js/router/api/Board/global.ROUTER.api.Board.detail_view_by__id.js
+++ js/router/api/Board/global.ROUTER.api.Board.detail_view_by__id.js
@@ -3,7 +3,7 @@ global.ROUTER.api.Common.detail_view_by__id = function( req, res ){
 	var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	req.url ).replace(/^.*\?/, '') );
 	console.log( _q );
 	global.api.Session.session_check(req, res, _q.sid, function( result ){
-
+		console.log( result )
 		if( result == 0 )
 		{
 			global.api.Response.res_200_ok_String( req, res, result.toString());
@@ -16,7 +16,7 @@ global.ROUTER.api.Common.detail_view_by__id = function( req, res ){
 				global.CSJLog.log("Connected correctly to server");
 
 				//------------------------------;
-
+				console.log( _q._id  )
 				var db0 = db.db('board');
 
 				db0.collection("notice").find( { _id : _q._id } ).toArray(function(err, doc){
