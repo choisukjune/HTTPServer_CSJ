b9ce8a6f19513b3ef6cc334e6bb2a61325ed2f3c
choisukjune <sukjune.choi@gmail.com>
2018-03-06 11:12:53 +0900

    asdfasdf

--- js/router/api/Board/global.ROUTER.api.Board.write.js
+++ js/router/api/Board/global.ROUTER.api.Board.write.js
@@ -5,12 +5,11 @@ global.ROUTER.api.Board.write = function( req, res ){
 		body +=	data;
 	   // global.CSJLog.timeStamp("Partial body: " + body);
 	})
+
 	req.on('end', function () {
 
-		console.log( body )
 		var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	body ).replace(/^.*\?/, '') );
 
-		console.log( _q )
 		global.api.Session.session_check(req, res, _q.sid, function( result ){
 
 			if( result == 0 )
@@ -24,13 +23,10 @@ global.ROUTER.api.Board.write = function( req, res ){
 
 					global.CSJLog.log("Connected correctly to server");
 
-
-					//------------------------------;
-
 					var db0 = db.db('board');
-
 					db0.collection("notice").find({}).sort({_id : -1}).limit(1).toArray(function(err,doc){
-						console.log(doc)
+
+						//ToDo function 으로 분리하기;
 						var Long = require('mongodb').Long;
 
 						var d = new Date();
@@ -44,30 +40,24 @@ global.ROUTER.api.Board.write = function( req, res ){
 						];
 
 
-						if( doc.length == 0 ){
-							var idx = 0
-						}else{
-							var idx = doc[ 0 ]._id + 1
-						}
+						if( doc.length == 0 ) var idx = 0
+						else var idx = doc[ 0 ]._id + 1
 
 						var doc = {
-							_id : idx,
-							title : _q.title,
-							content : _q.data,
-							regist_date : r,
-							modify_date : null,
-							delete_date : null
+							_id : idx
+							, _d : Long( -1 ).toInt()
+							, title : _q.title
+							, content : _q.data
+							, regist_date : r
+							, modify_date : null
+							, delete_date : null
 						}
 
 						db0.collection("notice").insert(doc,function(d){
-							console.log( d )
 							global.api.Response.res_200_ok_String( req, res, JSON.stringify( d ) );
 							db.close();
-
 						});
 					});
-					//------------------------------;
-
 				});
 			}
 		})
