2ee3090bb9de29f8dc6752da715390629d5e2934
choisukjune <sukjune.choi@gmail.com>
2018-03-07 10:49:56 +0900

    asdfasdf

---
+++ js/router/api/Board/global.ROUTER.api.Board.update.js
@@ -0,0 +1,71 @@
+global.ROUTER.api.Board.update = function( req, res ){
+
+		var	body = '';
+		req.on('data', function	(data) {
+			body +=	data;
+		   // global.CSJLog.timeStamp("Partial body: " + body);
+		})
+
+		req.on('end', function () {
+
+			var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	body ).replace(/^.*\?/, '') );
+
+			global.api.Session.session_check(req, res, _q.sid, function( result ){
+
+				if( result == 0 )
+				{
+					global.api.Response.res_200_ok_String( req, res, result.toString());
+
+				}
+				else
+				{
+					global.api.REQUIRES.MongoDB.MongoClient.connect(global.DB.CONFIG.driver_connect_url , function(err, db) {
+
+						//ToDo function 으로 분리하기;
+						var tagsToReplace = {
+						    '"': '&quot;',
+						    '&': '&amp;',
+						    '<': '&lt;',
+						    '>': '&gt;',
+						    "'": '&#039;'
+						};
+
+						function replaceTag(tag) {
+						    var s = tagsToReplace[tag] || tag;
+						    return s;
+						}
+
+						function safe_tags_replace(str) {
+						    return str.replace(/[&<>\"\'\{\}]/g, replaceTag);
+						}
+
+						global.CSJLog.log("Connected correctly to server");
+
+						var db0 = db.db('board');
+
+						//ToDo function 으로 분리하기;
+						var Long = require('mongodb').Long;
+
+						var d = new Date();
+						var r = [
+							Long( d.getFullYear() ).toInt()
+							, Long( d.getMonth() + 1 ).toInt()
+							, Long( d.getDate() ).toInt()
+							, Long( d.getHours() ).toInt()
+							, Long( d.getMinutes() ).toInt()
+							, Long( d.getSeconds() ).toInt()
+						];
+
+						var _query = { _id : _q.id };
+						var doc = { $set : {title : _q.title, content : _q.data, modify_date : r} };
+
+						db0.collection("notice").update(_query,doc,function(d){
+							global.api.Response.res_200_ok_String( req, res, JSON.stringify( doc ) );
+							db.close();
+						});
+					});
+				}
+			})
+		})
+
+};
