58d631b7be6e569e22ebe64c7794ec89d6025125
choisukjune <sukjune.choi@gmail.com>
2018-03-08 10:26:41 +0900

    asdfasdf

--- server/router.js
+++ server/router.js
@@ -120,7 +120,7 @@ global.ROUTER.Initialize_router	= function(	param ){
 console.log( path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a4[ z ])
 					//*/
 					// 화면을 그리는 용도로만 사용되므로 라우터가따로 존재할 필요없음;
-					t =	global.REQUIRES.fs.readFileSync(path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a4[ z ]"utf8");
+					t =	global.REQUIRES.fs.readFileSync(path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a4[ z ],"utf8");
 					console.log( t )
 					console.log(global.ROUTER.INFO )
 					//t = global.REQUIRES.fs.readFileSync(global.ROOTPath + "/js/api/HTML/global.api.HTML.render_html.js","utf8");
@@ -131,7 +131,7 @@ console.log( path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a4[ z ])
 					//global.ROUTER.INFO[ routerNm ] = require(	path + a0[ i ] );
 					//*/
 
-					console.log( "-[E]- Initialize_router_" + param + "-- " + path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a2[ k ] );
+					console.log( "-[E]- Initialize_router_" + param + "-- " + path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a4[ z ] );
 
 				}
 			}
