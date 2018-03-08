02d386edb6635bd8c31ff7c733447fae73f07ca1
choisukjune <sukjune.choi@gmail.com>
2018-03-08 10:36:03 +0900

    asdfasdf

--- server/router.js
+++ server/router.js
@@ -116,13 +116,14 @@ global.ROUTER.Initialize_router	= function(	param ){
 					_temp.pop();
 					_temp = _temp.pop();
 
-					routerNm = "/" + param + "/" + a0[ i ] + "/" + a1[ j ] + "/" + _temp;
+					routerNm = "/" + param + "/" + a0[ i ] + "/" + a1[ j ] + "/" + a4[ z ] + "/" + _temp;
 //*/
 					// 화면을 그리는 용도로만 사용되므로 라우터가따로 존재할 필요없음;
 					t =	global.REQUIRES.fs.readFileSync(path + '/' + a0[ i ] + '/' + a1[ j ] + '/' + a4[ z ],"utf8");
 					//console.log( t )
 					//console.log(global.ROUTER.INFO )
 					//t = global.REQUIRES.fs.readFileSync(global.ROOTPath + "/js/api/HTML/global.api.HTML.render_html.js","utf8");
+					console.log( routerNm )
 					global.ROUTER.INFO[	routerNm ] = e( t );
 
 					/*/
