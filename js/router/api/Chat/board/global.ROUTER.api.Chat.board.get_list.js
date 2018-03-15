ae396a9f2133474e337bcea7725e18242f8d58bc
choisukjune <sukjune.choi@gmail.com>
2018-03-15 16:02:25 +0900

    asdfasdf

--- js/router/api/Chat/board/global.ROUTER.api.Chat.board.get_list.js
+++ js/router/api/Chat/board/global.ROUTER.api.Chat.board.get_list.js
@@ -18,7 +18,7 @@ global.ROUTER.api.Chat.board.get_list = function( req, res ){
 
 				var db0 = db.db('chat');
 				var _query = { _d : { $ne : 0},cd$doc : _q.cd$doc }
-				db0.collection("board").find(_query).sort({ _id : 1 }).toArray(function(err, doc){
+				db0.collection("board").find(_query).sort({ _id : 1 }).limit( _q.limit ).skip( _q.skip ).toArray(function(err, doc){
 					global.api.Response.res_200_ok_String( req, res, JSON.stringify( doc ));
 					db.close();
 				});
