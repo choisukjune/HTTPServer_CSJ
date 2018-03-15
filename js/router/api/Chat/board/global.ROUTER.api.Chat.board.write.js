ae477cb95a2a9043bd22ed6b1a753fea748a384f
choisukjune <sukjune.choi@gmail.com>
2018-03-15 13:49:45 +0900

    asdfasdf

--- js/router/api/Chat/board/global.ROUTER.api.Chat.board.write.js
+++ js/router/api/Chat/board/global.ROUTER.api.Chat.board.write.js
@@ -59,12 +59,15 @@ global.ROUTER.api.Chat.board.write = function( req, res ){
 						, cd : ""
 						, cd$project : _q.cd$project
 						, cd$notebook : _q.cd$notebook
+						, cd$doc : _q.cd$doc
 						, content : _q.content
 						, regist_date : r
 						, delete_date : null
 					}
 
 					var db0 = db.db('chat');
+					var db1 = db.db('member');
+
 					db0.collection("board").find({}).sort({_id : -1}).limit(1).toArray(function(err,result){
 
 						if( result.length == 0 ) var idx = 0
@@ -74,11 +77,29 @@ global.ROUTER.api.Chat.board.write = function( req, res ){
 						db0.collection("board").count({ cd$notebook : _q.cd$notebook },function(err,result){
 
 							var doc_idx = result;
-							doc.cd = _q.cd$notebook + "-CHAT" + doc_idx;
+							doc.cd = _q.cd$doc + "-CHAT" + doc_idx;
+							db1.collection("member_basic").find({mid : _q.mid}).toArray(function(result){
+								
+								/*
+								{
+									"_id" : 0,
+									"id" : "csj6311@naver.com",
+									"pwd" : "a",
+									"first_Nm" : "Choi",
+									"last_Nm" : "Sukjune",
+									"sid" : "655d451b83494e91b269506c87087bb5f1e4c7f0cc206fb114dcc63f8371fd4d"
+								}
+								*/
 
-							db0.collection("board").insert(doc,function(d){
-								global.api.Response.res_200_ok_String( req, res, JSON.stringify( doc ) );
-								db.close();
+								var member_info = result[ 0 ];
+								doc.member._id = Long( member_info._id ).toInt();
+								doc.member._nm = member_info.first_Nm + " " + member_info.last_Nm;
+								doc.member.image = "https://semantic-ui.com/images/avatar/small/joe.jpg"
+
+								db0.collection("board").insert(doc,function(d){
+									global.api.Response.res_200_ok_String( req, res, JSON.stringify( doc ) );
+									db.close();
+								});
 							});
 						})
 					});
