05056cf92dc731559be0e810b770a3c81576e9c7
choisukjune <sukjune.choi@gmail.com>
2018-03-19 08:55:19 +0900

    asdfasdfsad

---
+++ js/router/api/Render/thtml/global.ROUTER.api.Category.notebook.delete.js
@@ -0,0 +1,27 @@
+global.ROUTER.api.Render.thtml.include_thtml= function( req, res ){
+
+	var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	req.url ).replace(/^.*\?/, '') );
+
+	global.api.Session.session_check(req, res, _q.sid, function( result ){
+
+		if( result == 0 )
+		{
+			global.api.Response.res_200_ok_String( req, res, result.toString());
+
+		}
+		else
+		{
+				//------------------------------;
+
+
+				var _path = global.ROUTER.api.Render.PATH.CONFIG;
+
+				var d = global.REQUIRES.fs.readFileSync( _path + _q.fileNm, "utf8")
+
+				global.api.Response.res_200_ok_String( req, res, d);
+
+
+				//------------------------------;
+		}
+	})
+};
---
+++ js/router/api/__define/001_global.ROUTER.api.Render.js
@@ -0,0 +1,13 @@
+//------------------------------;
+
+global.ROUTER.api.Render = {};
+//------------------------------;
+
+global.ROUTER.api.Render = {};
+global.ROUTER.api.Render.PATH.CONFIG = {};
+var	_path = global.ROOTPath + "/public/html/thtml/";
+
+var	_ =	global.ROUTER.api.Render.PATH.CONFIG ;
+_.__defineGetter__(	"_path", function(){ return _path;	} );
+
+//------------------------------;
