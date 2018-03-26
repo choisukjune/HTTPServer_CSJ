edae45170a05e70d89ef3582bc7c989a72932f44
choisukjune <sukjune.choi@gmail.com>
2018-03-26 23:47:43 +0900

    sdfsfsf

--- js/router/api/Board/notice/global.ROUTER.api.Board.notice.write.js
+++ js/router/api/Board/notice/global.ROUTER.api.Board.notice.write.js
@@ -9,7 +9,7 @@ global.ROUTER.api.Board.notice.write = function( req, res ){
 	req.on('end', function () {
 
 		var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	body ).replace(/^.*\?/, '') );
-
+console.log(_q)
 		global.api.Session.session_check(req, res, _q.sid, function( result ){
 
 			if( result == 0 )
