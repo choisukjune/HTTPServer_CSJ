433583eb17d7a433b21b05cf0412ca7ca0e125e3
choisukjune <sukjune.choi@gmail.com>
2018-03-27 15:50:56 +0900

    sdfsdf

--- js/api/Session/global.api.Session.session_check.js
+++ js/api/Session/global.api.Session.session_check.js
@@ -12,9 +12,8 @@ global.api.Session.session_check = function( req, res, sid, _cbFunction ){
 		r.auth( global.REDIS.CONFIG.pass );
 		// global.CSJLog.timeStamp( "Session - ", _p.sid);
 		r.get( sid, function(err, data){
-			global.CSJlog.timeStamp( sid )
-			global.CSJlog.timeStamp( data )
-			global.CSJLog.timeStamp('server has started.');
+			global.CSJLog.timeStamp( sid )
+			global.CSJLog.timeStamp( data )
 			//global.CSJLog.timeStamp( "Session__data - ", JSON.stringify( data ));
 			//global.api.Response.res_200_ok_String( req, res, JSON.stringify( data ))
 			_cbFunction( JSON.stringify( data ) )
