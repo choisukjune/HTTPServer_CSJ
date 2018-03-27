4df836d5396d4a91505432a76499f5bea1b8d276
choisukjune <sukjune.choi@gmail.com>
2018-03-27 17:27:33 +0900

    sdfsdfsdfsd

--- js/router/api/Board/notice/global.ROUTER.api.Board.notice.update.js
+++ js/router/api/Board/notice/global.ROUTER.api.Board.notice.update.js
@@ -10,7 +10,7 @@ global.ROUTER.api.Board.notice.update = function( req, res ){
 
 			var	_q = global.REQUIRES.querystring.parse(	decodeURI( body ) );
 
-			global.CSJLog.timeStamp(_q)
+			global.CSJLog.timeStamp( JSON.stringify(_q) );
 			
 			global.api.Session.session_check(req, res, _q.sid, function( result ){
 
