f3fab84d720fb50d22597c8d50b3b43b20c53fc3
choisukjune <sukjune.choi@gmail.com>
2018-04-02 16:08:24 +0900

    sdfsfsdf

--- js/router/api/Category/project/global.ROUTER.api.Category.project.update.js
+++ js/router/api/Category/project/global.ROUTER.api.Category.project.update.js
@@ -57,7 +57,7 @@ global.ROUTER.api.Category.project.update = function( req, res ){
 						];
 
 						var _query = { _id : Long( _q._id ).toInt() };
-						var doc = { $set : {title : _q.title, content : _q.data, modify_date : r} };
+						var doc = { $set : {nm : _q.nm, description : _q.description, modify_date : r} };
 
 						console.log( _query )
 						console.log( doc )
