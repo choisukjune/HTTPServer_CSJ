a3e93229741074a5c8378a640770b613c966d24e
choisukjune <sukjune.choi@gmail.com>
2018-04-06 08:48:41 +0900

    asdasd

--- server/router.js
+++ server/router.js
@@ -215,17 +215,103 @@ global.ROUTER.routerControl	= function(req,res)	{
 			});
 			// resource.on('close',	function(){	console.log( "---------- close ----------" ); });
 
+
+			var CONTENTTYPES = {
+				'aac': 'audio/aac',
+				'ai': 'application/postscript',
+				'appcache': 'text/cache-manifest',
+				'avi': 'video/avi',
+				'bin': 'application/octet-stream',
+				'bmp': 'image/bmp',
+				'coffee': 'text/coffeescript',
+				'css': 'text/css',
+				'csv': 'text/csv',
+				'doc': 'application/msword',
+				'docx': 'application/msword',
+				'dtd': 'application/xml-dtd',
+				'eps': 'application/postscript',
+				'exe': 'application/octet-stream',
+				'flac': 'audio/x-flac',
+				'geojson': 'application/json',
+				'gif': 'image/gif',
+				'gzip': 'application/x-gzip',
+				'htm': 'text/html',
+				'html': 'text/html',
+				'ico': 'image/x-icon',
+				'ics': 'text/calendar',
+				'ifb': 'text/calendar',
+				'jpe': 'image/jpeg',
+				'jpeg': 'image/jpeg',
+				'jpg': 'image/jpeg',
+				'js': 'text/javascript',
+				'json': 'application/json',
+				'jsx': 'text/jsx',
+				'less': 'text/css',
+				'm4a': 'audio/mp4a-latm',
+				'm4v': 'video/x-m4v',
+				'manifest': 'text/cache-manifest',
+				'md': 'text/x-markdown',
+				'mid': 'audio/midi',
+				'midi': 'audio/midi',
+				'mov': 'video/quicktime',
+				'mp3': 'audio/mpeg',
+				'mp4': 'video/mp4',
+				'mpe': 'video/mpeg',
+				'mpeg': 'video/mpeg',
+				'mpg': 'video/mpeg',
+				'mpga': 'audio/mpeg',
+				'mtl': 'text/plain',
+				'mv4': 'video/mv4',
+				'obj': 'text/plain',
+				'ogg': 'application/ogg',
+				'ogv': 'video/ogg',
+				'package': 'text/plain',
+				'pdf': 'application/pdf',
+				'png': 'image/png',
+				'ppt': 'application/vnd.ms-powerpoint',
+				'pptx': 'application/vnd.ms-powerpoint',
+				'ps': 'application/postscript',
+				'rar': 'application/x-rar-compressed',
+				'rtf': 'text/rtf',
+				'sass': 'text/css',
+				'scss': 'text/css',
+				'sh': 'application/x-sh',
+				'stl': 'application/sla',
+				'svg': 'image/svg+xml',
+				'swf': 'application/x-shockwave-flash',
+				'tar': 'application/x-tar',
+				'tif': 'image/tiff',
+				'tiff': 'image/tiff',
+				'txt': 'text/plain',
+				'wav': 'audio/x-wav',
+				'webm': 'video/webm',
+				'webp': 'image/webp',
+				'woff': 'application/font-woff',
+				'woff2': 'application/font-woff2',
+				'xht': 'application/xhtml+xml',
+				'xhtml': 'application/xhtml+xml',
+				'xls': 'application/vnd.ms-excel',
+				'xlsx': 'application/vnd.ms-excel',
+				'xml': 'application/xml',
+				'xpm': 'image/x-xpixmap',
+				'xsl': 'application/xml',
+				'xslt': 'application/xslt+xml',
+				'zip': 'application/zip'
+			};
+			
+			res.writeHeader(200, {"Content-Type":	CONTENTTYPES[ extension ]})
+
 			// set content type
-			if (extension === 'html') res.writeHeader(200, {"Content-Type":	'text/html'});
-			else if	(extension === 'htm') res.writeHeader(200, {"Content-Type":	'text/html'});
-			else if	(extension === 'css') res.writeHeader(200, {"Content-Type":	'text/css'});
-			else if	(extension === 'js') res.writeHeader(200, {"Content-Type": 'text/javascript'});
-			else if	(extension === 'png') res.writeHeader(200, {"Content-Type":	'image/png'});
-			else if	(extension === 'jpg') res.writeHeader(200, {"Content-Type":	'image/jpg'});
-			else if	(extension === 'woff') res.writeHeader(200,	{"Content-Type": 'application/font-woff'});
-			else if	(extension === 'woff2')	res.writeHeader(200, {"Content-Type": 'application/font-woff2'});
-			else if	(extension === 'ttf') res.writeHeader(200, {"Content-Type":	'application/x-font-ttf'});
-			else if	(extension === 'jpeg') res.writeHeader(200,	{"Content-Type": 'image/jpeg'});
+			// if (extension === 'html') res.writeHeader(200, {"Content-Type":	'text/html'});
+			// else if	(extension === 'htm') res.writeHeader(200, {"Content-Type":	'text/html'});
+			// else if	(extension === 'css') res.writeHeader(200, {"Content-Type":	'text/css'});
+			// else if	(extension === 'js') res.writeHeader(200, {"Content-Type": 'text/javascript'});
+			// else if	(extension === 'png') res.writeHeader(200, {"Content-Type":	'image/png'});
+			// else if	(extension === 'jpg') res.writeHeader(200, {"Content-Type":	'image/jpg'});
+			// else if	(extension === 'woff') res.writeHeader(200,	{"Content-Type": 'application/font-woff'});
+			// else if	(extension === 'woff2')	res.writeHeader(200, {"Content-Type": 'application/font-woff2'});
+			// else if	(extension === 'ttf') res.writeHeader(200, {"Content-Type":	'application/x-font-ttf'});
+			// else if	(extension === 'jpeg') res.writeHeader(200,	{"Content-Type": 'image/jpeg'});
 
 			resource.pipe(res);
 			return;
