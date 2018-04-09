58d62a183fb22238d39aa8fc24db92e77ed798f6
choisukjune <sukjune.choi@gmail.com>
2018-04-09 17:26:37 +0900

    sdfsdf

--- js/router/api/File/board/global.ROUTER.api.File.board.get_list.js
+++ js/router/api/File/board/global.ROUTER.api.File.board.get_list.js
@@ -1,6 +1,9 @@
 global.ROUTER.api.File.board.get_list = function( req, res ){
 
-	var	_q = global.REQUIRES.querystring.parse(	decodeURIComponent(	req.url ).replace(/^.*\?/, '') );
+	var	_q = global.REQUIRES.querystring.parse(	global.REQUIRES.url.parse( decodeURIComponent(	req.url ) ).query );
+	
+	global.CSJLog.timeStamp( JSON.stringify( _q ) );
+	
 	global.api.Session.session_check(req, res, _q.sid, function( result ){
 
 		if( result == 0 )
@@ -12,16 +15,12 @@ global.ROUTER.api.File.board.get_list = function( req, res ){
 		{
 			global.api.REQUIRES.MongoDB.MongoClient.connect(global.DB.CONFIG.driver_connect_url , function(err, db) {
 
-				global.CSJLog.log("Connected correctly to server");
-				var Long = require('mongodb').Long;
+				global.CSJLog.timeStamp("Connected correctly to server");
 				//------------------------------;
 
-				var db0 = db.db('chat');
+				var db0 = db.db('file');
 				var _query = { _d : { $ne : 0},cd$doc : _q.cd$doc }
-				db0.collection("board").find(_query).sort({ _id : -1 })
-					.limit( Long( _q.limit ).toInt() )
-					.skip( Long( _q.skip ).toInt() )
-					.toArray(function(err, doc){
+				db0.collection("board").find(_query).sort({ _id : -1 }).toArray(function(err, doc){
 						global.api.Response.res_200_ok_String( req, res, JSON.stringify( doc ));
 						db.close();
 				});
