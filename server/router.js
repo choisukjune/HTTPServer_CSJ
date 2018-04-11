6dc4a375c2aaa6aa306b39c1734158f50276c7c0
choisukjune <sukjune.choi@gmail.com>
2018-04-11 15:37:54 +0900

    sdfsdf

--- server/router.js
+++ server/router.js
@@ -145,22 +145,93 @@ global.ROUTER.Initialize_router	= function(	param ){
 
 global.ROUTER.routerControl	= function(req,res)	{
 
-	var	resource_info = [
-		'html'
-		, 'htm'
-		, 'css'
-		, 'js'
-		, 'png'
-		, 'jpg'
-		, 'woff'
-		, 'woff2'
-		, 'ttf'
-		, 'jpeg'
-	];
+	var CONTENTTYPES = {
+		'aac': 'audio/aac',
+		'ai': 'application/postscript',
+		'appcache': 'text/cache-manifest',
+		'avi': 'video/avi',
+		'bin': 'application/octet-stream',
+		'bmp': 'image/bmp',
+		'coffee': 'text/coffeescript',
+		'css': 'text/css',
+		'csv': 'text/csv',
+		'doc': 'application/msword',
+		'docx': 'application/msword',
+		'dtd': 'application/xml-dtd',
+		'eps': 'application/postscript',
+		'exe': 'application/octet-stream',
+		'flac': 'audio/x-flac',
+		'geojson': 'application/json',
+		'gif': 'image/gif',
+		'gzip': 'application/x-gzip',
+		'htm': 'text/html',
+		'html': 'text/html',
+		'ico': 'image/x-icon',
+		'ics': 'text/calendar',
+		'ifb': 'text/calendar',
+		'jpe': 'image/jpeg',
+		'jpeg': 'image/jpeg',
+		'jpg': 'image/jpeg',
+		'js': 'text/javascript',
+		'json': 'application/json',
+		'jsx': 'text/jsx',
+		'less': 'text/css',
+		'm4a': 'audio/mp4a-latm',
+		'm4v': 'video/x-m4v',
+		'manifest': 'text/cache-manifest',
+		'md': 'text/x-markdown',
+		'mid': 'audio/midi',
+		'midi': 'audio/midi',
+		'mov': 'video/quicktime',
+		'mp3': 'audio/mpeg',
+		'mp4': 'video/mp4',
+		'mpe': 'video/mpeg',
+		'mpeg': 'video/mpeg',
+		'mpg': 'video/mpeg',
+		'mpga': 'audio/mpeg',
+		'mtl': 'text/plain',
+		'mv4': 'video/mv4',
+		'obj': 'text/plain',
+		'ogg': 'application/ogg',
+		'ogv': 'video/ogg',
+		'package': 'text/plain',
+		'pdf': 'application/pdf',
+		'png': 'image/png',
+		'ppt': 'application/vnd.ms-powerpoint',
+		'pptx': 'application/vnd.ms-powerpoint',
+		'ps': 'application/postscript',
+		'rar': 'application/x-rar-compressed',
+		'rtf': 'text/rtf',
+		'sass': 'text/css',
+		'scss': 'text/css',
+		'sh': 'application/x-sh',
+		'stl': 'application/sla',
+		'svg': 'image/svg+xml',
+		'swf': 'application/x-shockwave-flash',
+		'tar': 'application/x-tar',
+		'tif': 'image/tiff',
+		'tiff': 'image/tiff',
+		'txt': 'text/plain',
+		'wav': 'audio/x-wav',
+		'webm': 'video/webm',
+		'webp': 'image/webp',
+		'woff': 'application/font-woff',
+		'woff2': 'application/font-woff2',
+		'xht': 'application/xhtml+xml',
+		'xhtml': 'application/xhtml+xml',
+		'xls': 'application/vnd.ms-excel',
+		'xlsx': 'application/vnd.ms-excel',
+		'xml': 'application/xml',
+		'xpm': 'image/x-xpixmap',
+		'xsl': 'application/xml',
+		'xslt': 'application/xslt+xml',
+		'zip': 'application/zip'
+	};
 
 	var	pathname = global.REQUIRES.url.parse(req.url).pathname;
 	var	tmp = req.url.lastIndexOf(".");
 	var	extension  = req.url.substring((tmp	+ 1));
+	console.log( extension )
 	var resource_path_key = req.url.replace('/web',"")
 
 	if( extension.indexOf("?") != -1 ){
@@ -200,7 +271,7 @@ global.ROUTER.routerControl	= function(req,res)	{
 		var resource_dir = global.ROOTPath + "/public/" + path.split("?")[0]
 	}
 
-	if ( resource_info.indexOf( extension ) !== -1 ) {
+	if ( CONTENTTYPES[ extension ] ) {
 		//
 		//if( req.url.indexOf( "?" ) == -1){
 			//var resource = global.REQUIRES.fs.createReadStream(global.ROOTPath + "/public/" +	req.url, 'utf8');
@@ -215,90 +286,6 @@ global.ROUTER.routerControl	= function(req,res)	{
 			});
 			// resource.on('close',	function(){	console.log( "---------- close ----------" ); });
 
-
-			var CONTENTTYPES = {
-				'aac': 'audio/aac',
-				'ai': 'application/postscript',
-				'appcache': 'text/cache-manifest',
-				'avi': 'video/avi',
-				'bin': 'application/octet-stream',
-				'bmp': 'image/bmp',
-				'coffee': 'text/coffeescript',
-				'css': 'text/css',
-				'csv': 'text/csv',
-				'doc': 'application/msword',
-				'docx': 'application/msword',
-				'dtd': 'application/xml-dtd',
-				'eps': 'application/postscript',
-				'exe': 'application/octet-stream',
-				'flac': 'audio/x-flac',
-				'geojson': 'application/json',
-				'gif': 'image/gif',
-				'gzip': 'application/x-gzip',
-				'htm': 'text/html',
-				'html': 'text/html',
-				'ico': 'image/x-icon',
-				'ics': 'text/calendar',
-				'ifb': 'text/calendar',
-				'jpe': 'image/jpeg',
-				'jpeg': 'image/jpeg',
-				'jpg': 'image/jpeg',
-				'js': 'text/javascript',
-				'json': 'application/json',
-				'jsx': 'text/jsx',
-				'less': 'text/css',
-				'm4a': 'audio/mp4a-latm',
-				'm4v': 'video/x-m4v',
-				'manifest': 'text/cache-manifest',
-				'md': 'text/x-markdown',
-				'mid': 'audio/midi',
-				'midi': 'audio/midi',
-				'mov': 'video/quicktime',
-				'mp3': 'audio/mpeg',
-				'mp4': 'video/mp4',
-				'mpe': 'video/mpeg',
-				'mpeg': 'video/mpeg',
-				'mpg': 'video/mpeg',
-				'mpga': 'audio/mpeg',
-				'mtl': 'text/plain',
-				'mv4': 'video/mv4',
-				'obj': 'text/plain',
-				'ogg': 'application/ogg',
-				'ogv': 'video/ogg',
-				'package': 'text/plain',
-				'pdf': 'application/pdf',
-				'png': 'image/png',
-				'ppt': 'application/vnd.ms-powerpoint',
-				'pptx': 'application/vnd.ms-powerpoint',
-				'ps': 'application/postscript',
-				'rar': 'application/x-rar-compressed',
-				'rtf': 'text/rtf',
-				'sass': 'text/css',
-				'scss': 'text/css',
-				'sh': 'application/x-sh',
-				'stl': 'application/sla',
-				'svg': 'image/svg+xml',
-				'swf': 'application/x-shockwave-flash',
-				'tar': 'application/x-tar',
-				'tif': 'image/tiff',
-				'tiff': 'image/tiff',
-				'txt': 'text/plain',
-				'wav': 'audio/x-wav',
-				'webm': 'video/webm',
-				'webp': 'image/webp',
-				'woff': 'application/font-woff',
-				'woff2': 'application/font-woff2',
-				'xht': 'application/xhtml+xml',
-				'xhtml': 'application/xhtml+xml',
-				'xls': 'application/vnd.ms-excel',
-				'xlsx': 'application/vnd.ms-excel',
-				'xml': 'application/xml',
-				'xpm': 'image/x-xpixmap',
-				'xsl': 'application/xml',
-				'xslt': 'application/xslt+xml',
-				'zip': 'application/zip'
-			};
-			
 			res.writeHeader(200, {"Content-Type":	CONTENTTYPES[ extension ]})
 
 			// set content type
