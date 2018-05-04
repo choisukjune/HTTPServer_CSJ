96541e0698e4a15989827f7288eb0aa044056f5b
choisukjune <sukjune.choi@gmail.com>
2018-05-04 16:03:12 +0900

    test

--- MongoDB/MongoDB_Start.bat
+++ MongoDB/MongoDB_Start.bat
@@ -1,3 +1,3 @@
-del /q Log\0\.
+:del /q Log\0\.
 
-mongod --config /home/sukjune/githup/httpServer_csj/MongoDB/Config/0.conf
+D:\GitHub_CSJ\MongoDB\bin\mongod.exe --config D:\GitHub_CSJ\httpServer_csj\MongoDB\Config\0.conf
