c90a79e77724d2963f4b410191378eaf19e14ac2
choisukjune <sukjune.choi@gmail.com>
2018-05-04 10:40:42 +0900

    ttt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-global.DB = {}
-global.DB.Info = { mongoPath : "mongo", MongoDBServerURL : "127.0.0.1:55555/admin", ID : "tjrwns", PASS : "q1q1q1q1q1" }
-global.DB.CONFIG = {}
+global.DB = {};
+global.DB.Info = { mongoPath : "mongo", MongoDBServerURL : "127.0.0.1:55555/admin", ID : "tjrwns", PASS : "q1q1q1q1q1" };
+global.DB.CONFIG = {};
 global.DB.CONFIG.driver_connect_url = 'mongodb://' + global.DB.Info.ID + ':' + global.DB.Info.PASS + '@127.0.0.1:55555/admin';
 global.DB.CONFIG.console_excute_command_js = [ global.DB.Info.mongoPath, global.DB.Info.MongoDBServerURL, '-u', global.DB.Info.ID, '-p', global.DB.Info.PASS, '<' ];
 global.DB.CONFIG.console_excute_command_eval = [ global.DB.Info.mongoPath, global.DB.Info.MongoDBServerURL, '-u', global.DB.Info.ID, '-p', global.DB.Info.PASS, '--eval' ];
@@ -18,4 +18,3 @@ require('./server/router');
 
 //server.start(router.routerControl);
 global.SERVER.serverStart( global.ROUTER.routerControl );
-console.log("깃헙멀티계정테스트---7")
