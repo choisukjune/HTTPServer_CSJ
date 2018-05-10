9604d04b132764b7a1d4e11f408c9d61e4061eef
choisukjune <sukjune.choi@gmail.com>
2018-05-10 17:19:25 +0900

    dfgdfgdfg

--- js/api/Request/global.api.Request.req_POST_https.js
+++ js/api/Request/global.api.Request.req_POST_https.js
@@ -4,7 +4,8 @@ global.api.Request.request__POST_https = function( o, characterSet, _cb ){
 //	var _q = JSON.parse( data.data )
 //
 	console.log( o )
-	var postBody = global.REQUIRES.querystring.escape(JSON.stringify(o.data));
+	//var postBody = global.REQUIRES.querystring.escape(JSON.stringify(o.data));
+	var postBody = o.data;
 	console.log( postBody )
 	var options = {
 		hostname: o.host
@@ -12,10 +13,22 @@ global.api.Request.request__POST_https = function( o, characterSet, _cb ){
 		, path: o.path
 		, method: "POST"
 		, headers : {
-		   'Content-Type': 'application/x-www-form-urlencoded',
+		   'Content-Type': 'application/json',
 		   'Content-Length': postBody.length
+		   'X-Access-Key': '5af3fe773fcc2fa8',
+		   'X-Access-Secret': 'fbf0ce1cf97738667abbfbabc0ec0b36'
 		}
 	};
+
+	// curl -X POST --header 'Content-Type: ' --header 'Accept: application/json' --header 'X-Access-Key: 5af3fe773fcc2fa8' --header 'X-Access-Secret: fbf0ce1cf97738667abbfbabc0ec0b36' -d '{ \
+	//      "message": "Your message", \
+	//      "botOption": { \
+	//          "actAsManager": false, \
+	//          "silentToManager": false, \
+	//          "silentToGuest": false \
+	//      } \
+	//  }' 'https://api.channel.io/open/user_chats/690905/messages?botName=sdfsdfsd'
+
 
 	var req = global.REQUIRES.https.request(o, function( res ){
 
