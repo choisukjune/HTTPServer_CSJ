ddfcb3123aafcf3439843e3e5807721b7c6482e4
choisukjune <sukjune.choi@gmail.com>
2018-05-10 18:44:35 +0900

    asdfasdf

--- js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
+++ js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
@@ -48,11 +48,12 @@ global.ROUTER.api.webhook.channelio.recieve_message = function( req, res ){
 			// }
 		};*/
 		console.log( o )
-
-		global.api.Request.request__POST_https(o,"utf8",function(d){
-			console.log( "=======================>",d )
-			global.api.Response.res_200_ok_String( req, res, "OK");
-		})
-
+		if( body.entity.personType != "Bot" )
+		{
+			global.api.Request.request__POST_https(o,"utf8",function(d){
+				console.log( "=======================>",d )
+				global.api.Response.res_200_ok_String( req, res, "OK");
+			})
+		}
 	})
 };
