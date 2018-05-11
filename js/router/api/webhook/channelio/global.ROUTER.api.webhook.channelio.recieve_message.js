a3f0cc61a0a0ccfa436ad075ff5793516b1c3756
choisukjune <sukjune.choi@gmail.com>
2018-05-11 10:57:10 +0900

    sdfgsdfg

--- js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
+++ js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
@@ -16,6 +16,7 @@ global.ROUTER.api.webhook.channelio.recieve_message = function( req, res ){
 
 		o.data.message = encodeURIComponent( "Your message is " + ob.entity.message )
 		global.CSJLog.log( JSON.stringify( o ) );
+		global.CSJLog.log( JSON.stringify( global.ROUTER.api.webhook.channelio.CONFIG._options ) );
 		if( ob.entity.personType != "Bot" )
 		{
 			global.api.Request.request__POST_https(o,"utf8",function(d){
