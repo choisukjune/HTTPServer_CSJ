9c92d96be0b1f8658e60ee952387098f180e28b9
choisukjune <sukjune.choi@gmail.com>
2018-05-11 11:08:48 +0900

    sdfgsdfgdfsg

--- js/router/api/__define/001_global.ROUTER.api.webhook.js
+++ js/router/api/__define/001_global.ROUTER.api.webhook.js
@@ -5,30 +5,15 @@ global.ROUTER.api.webhook = {};
 global.ROUTER.api.webhook.channelio = {};
 global.ROUTER.api.webhook.channelio.CONFIG = {};
 
-// var X_Access_Key = "5af3fe773fcc2fa8";
-// var X_Access_Secret = "fbf0ce1cf97738667abbfbabc0ec0b36"
-var _botname =  "Choisukjune__AAA";
-
-var _options = {
-    host: "api.channel.io"//o.host
-    , port: "443"//o.port
-    , path: "/open/user_chats/{{chatId}}/messages?botName={{botname}}"//o.path
-    , headers : {
-       'Content-Type': 'application/json'
-       , 'Content-Length': -1
-       , 'X-Access-Key': "5af3fe773fcc2fa8"
-       , 'X-Access-Secret': "fbf0ce1cf97738667abbfbabc0ec0b36"
-    }
-    , data : {
-        "message" : "",
-        "botOption": {
-            "actAsManager": false,
-            "silentToManager": false,
-            "silentToGuest": false
-        }
-    }
-}
+var _channelio_botname =  "Choisukjune__AAA";
+var _channelio_host = "api.channel.io";
+var _channelio_port = "443";
+var _channelio_X_Access_Key = "5af3fe773fcc2fa8";
+var _channelio_X_Access_Secret = "fbf0ce1cf97738667abbfbabc0ec0b36"
 
 var	_ =	global.ROUTER.api.webhook.channelio.CONFIG;
-_.__defineGetter__(	"_options", function(){ return _options;	} );
-_.__defineGetter__(	"_botname", function(){ return _botname;	} );
+_.__defineGetter__(	"_channelio_botname", function(){ return _channelio_botname;	} );
+_.__defineGetter__(	"_channelio_host", function(){ return _channelio_host;	} );
+_.__defineGetter__(	"_channelio_port", function(){ return _channelio_port;	} );
+_.__defineGetter__(	"_channelio_X_Access_Key", function(){ return _channelio_X_Access_Key;	} );
+_.__defineGetter__(	"_channelio_X_Access_Secret", function(){ return _channelio_X_Access_Secret;	} );
--- js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
+++ js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
@@ -9,12 +9,36 @@ global.ROUTER.api.webhook.channelio.recieve_message = function( req, res ){
 
 		var	_q = global.REQUIRES.querystring.parse(	decodeURI( body ) );
 		var ob = JSON.parse( body );
-		var o = global.ROUTER.api.webhook.channelio.CONFIG._options
-		global.CSJLog.log( body );
-		o.path = o.path.replace("{{chatId}}",ob.entity.chatId)
-				.replace("{{botname}}",global.ROUTER.api.webhook.channelio.CONFIG._botname);
+
+		// var _channelio_botname =  "Choisukjune__AAA";
+		// var _channelio_host = "api.channel.io";
+		// var _channelio_port = "443";
+		// var _channelio_X_Access_Key = "5af3fe773fcc2fa8";
+		// var _channelio_X_Access_Secret = "fbf0ce1cf97738667abbfbabc0ec0b36"
+
+
+		var o = {
+		    host : global.ROUTER.api.webhook.channelio.CONFIG._channelio_host//o.host
+		    , port : global.ROUTER.api.webhook.channelio.CONFIG._channelio_port//o.port
+		    , path : "/open/user_chats/" + ob.entity.chatId + "/messages?botName=" + global.ROUTER.api.webhook.channelio.CONFIG._channelio_botname//o.path
+		    , headers : {
+		       'Content-Type': 'application/json'
+		       , 'Content-Length': -1
+		       , 'X-Access-Key': global.ROUTER.api.webhook.channelio.CONFIG._channelio_botname
+		       , 'X-Access-Secret': global.ROUTER.api.webhook.channelio.CONFIG._channelio_botname
+		    }
+		    , data : {
+		        "message" : "",
+		        "botOption": {
+		            "actAsManager": false,
+		            "silentToManager": false,
+		            "silentToGuest": false
+		        }
+		    }
+		}
 
 		o.data.message = encodeURIComponent( "Your message is " + ob.entity.message )
+
 		global.CSJLog.log( JSON.stringify( o ) );
 		global.CSJLog.log( JSON.stringify( global.ROUTER.api.webhook.channelio.CONFIG._options ) );
 		if( ob.entity.personType != "Bot" )
