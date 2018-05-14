830f874d3cf5cce7f36fa67b297c405b0023cff8
choisukjune <sukjune.choi@gmail.com>
2018-05-14 16:03:14 +0900

    asdfasdfasdf

--- js/api/Request/global.api.Request.req_POST_https.js
+++ js/api/Request/global.api.Request.req_POST_https.js
@@ -5,6 +5,7 @@ global.api.Request.request__POST_https = function( o, characterSet, _cb ){
 //
 	//var postBody = global.REQUIRES.querystring.escape(JSON.stringify(o.data));
 	var postBody = JSON.stringify(o.data);
+	console.log( postBody )
 	var options = {
 		hostname: o.host
 		, port: o.port
@@ -12,7 +13,7 @@ global.api.Request.request__POST_https = function( o, characterSet, _cb ){
 		, method: "POST"
 		, headers : o.headers
 	};
-	options.headers[ 'Content-Length' ] = postBody.length;
+	options.headers[ 'Content-Length' ] = Buffer.byteLength( o.data );
 	// curl -X POST --header 'Content-Type: ' --header 'Accept: application/json' --header 'X-Access-Key: 5af3fe773fcc2fa8' --header 'X-Access-Secret: fbf0ce1cf97738667abbfbabc0ec0b36' -d '{ \
 	//      "message": "Your message", \
 	//      "botOption": { \
@@ -40,5 +41,6 @@ global.api.Request.request__POST_https = function( o, characterSet, _cb ){
 		global.CSJLog.error('problem with request: ${e.message}');
 	});
 	req.write( decodeURIComponent( postBody ) );
+console.log( decodeURIComponent( postBody ) )
 	req.end();
 };
