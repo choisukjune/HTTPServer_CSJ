32b23707569210878acfae4b5f29b20b3e10be02
choisukjune <sukjune.choi@gmail.com>
2018-05-14 16:07:16 +0900

    asdfasdf

--- js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
+++ js/router/api/webhook/channelio/global.ROUTER.api.webhook.channelio.recieve_message.js
@@ -67,7 +67,7 @@ global.ROUTER.api.webhook.channelio.recieve_message = function( req, res ){
 						"Content-Type" : "application/json"
 					}
 					, data : {
-						"body" : "안녕하세요~",
+						"body" : message,
 						"connectColor" : "#FAC11B",
 						"connectInfo" : [{
 							"title" : "Topping",
