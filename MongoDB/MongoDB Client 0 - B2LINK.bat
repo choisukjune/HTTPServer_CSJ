1c795d73e56b57f7e7eb47c3bf722561aba0935d
choisukjune <sukjune.choi@gmail.com>
2018-05-24 11:45:08 +0900

    d

--- MongoDB/MongoDB Client 0 - B2LINK.bat
+++ MongoDB/MongoDB Client 0 - B2LINK.bat
@@ -1,2 +1,2 @@
 D:\GitHub_CSJ\MongoDB\bin\mongo.exe --port 55555 -u tjrwns -p q1q1q1q1q1 --authenticationDatabase admin
-:db.createUser({user: "tjrwms",pwd: "q1q1q1q1q1",roles: [ { role: "root", db: "admin" } ]})
+:db.createUser({user: "tjrwns",pwd: "q1q1q1q1q1",roles: [ { role: "root", db: "admin" } ]})
