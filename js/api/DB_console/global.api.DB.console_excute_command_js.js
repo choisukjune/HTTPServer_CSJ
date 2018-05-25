0bfbb2b2bfe44bc0c4613f44ebe341844aa1b44d
choisukjune <sukjune.choi@gmail.com>
2018-05-25 15:38:46 +0900

    sdfsdfsdf

--- js/api/DB_console/global.api.DB.console_excute_command_js.js
+++ js/api/DB_console/global.api.DB.console_excute_command_js.js
@@ -3,7 +3,7 @@ global.api.DB_console.console_excute_command_js = function( dbjs, _cbFunction ){
 	var db_excute_command = [ global.DB.Info.MongoDBServerURL, '-u', global.DB.Info.ID, '-p', global.DB.Info.PASS ];
 // global.DB.Info.mongoPath,
 	db_excute_command.push( dbjs );
-console.log( db_excute_command );
+	global.CSJLog.timeStamp( "Command : " + db_excute_command.join(" ") );
 	var _exec = global.REQUIRES.child_process.spawnSync( 'mongo', db_excute_command );
 	var _tmp = _exec.stdout.toString('utf8')//.split("\n")
 	var r = _tmp//.slice(3, _tmp.length - 2).join("").replace(/\t/g, "").replace(/ : /g, ":")
