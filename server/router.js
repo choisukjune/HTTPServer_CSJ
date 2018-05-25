0adf94729ad0dc5372aad08cd6b20af74fe017d1
choisukjune <sukjune.choi@gmail.com>
2018-05-25 13:35:34 +0900

    asdf

--- server/router.js
+++ server/router.js
@@ -280,6 +280,7 @@ global.ROUTER.routerControl	= function(req,res)	{
 		if( _tmp0.indexOf("temp") !== -1)
 		{
 			var path = check_resource_res( _tmp0, "libs" );
+			console.log( path )
 			var resource_dir = global.ROOTPath + "/public/libs/mapchart/";
 			console.log( resource_dir )
 		}
