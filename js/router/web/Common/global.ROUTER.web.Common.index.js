f03618be2bca48d3cf7c746fb9c06481879de451
choisukjune <sukjune.choi@gmail.com>
2018-06-01 14:36:57 +0900

    sdfsdfsdf

--- js/router/web/Common/global.ROUTER.web.Common.index.js
+++ js/router/web/Common/global.ROUTER.web.Common.index.js
@@ -76,7 +76,7 @@ global.ROUTER.web.Common.index = function( req,res ){
 	 * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
 	 */
 	function listFiles(auth) {
-	  const drive = google.drive({version: 'v3', auth});
+	  var drive = google.google.drive({version: 'v3', auth});
 	  drive.files.list({
 	    pageSize: 10,
 	    fields: 'nextPageToken, files(id, name)',
