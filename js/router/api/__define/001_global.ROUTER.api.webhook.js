7922f477f7af8dac07b61d2a1fe2d91db2dd2ae1
choisukjune <sukjune.choi@gmail.com>
2018-08-24 10:50:26 +0900

    asdfasdf

--- js/router/api/__define/001_global.ROUTER.api.webhook.js
+++ js/router/api/__define/001_global.ROUTER.api.webhook.js
@@ -8,8 +8,8 @@ global.ROUTER.api.webhook.channelio.CONFIG = {};
 var _channelio_botname =  "Choisukjune__AAA";
 var _channelio_host = "api.channel.io";
 var _channelio_port = "443";
-var _channelio_X_Access_Key = "5af3fe773fcc2fa8";
-var _channelio_X_Access_Secret = "fbf0ce1cf97738667abbfbabc0ec0b36"
+var _channelio_X_Access_Key = "5b7f63a1cc665fc0";
+var _channelio_X_Access_Secret = "7efbe4ffb3d5ddfed6ef96e9f67a4e6d"
 
 var	_ =	global.ROUTER.api.webhook.channelio.CONFIG;
 _.__defineGetter__(	"_channelio_botname", function(){ return _channelio_botname;	} );
